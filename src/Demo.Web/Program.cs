9ace3a57e64ff1567307dea90326479ed18ab407
binoypatel <binoypatel@msn.com>
2016-09-06 14:33:34 -0400

    Add project files.

---
+++ src/Demo.Web/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Demo.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseStartup("Demo.Bootstrapper")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
